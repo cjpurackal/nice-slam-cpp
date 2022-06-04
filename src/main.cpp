46c439c8cef8dab5b78d4d0f4b0cac37a91d6854
cjpurackal <christeejacobs@gmail.com>
2022-06-04 23:59:06 +0530

    test

--- src/main.cpp
+++ src/main.cpp
@@ -2,26 +2,14 @@
 #include <torch/script.h>
 #include <iostream>
 #include <memory>
+#include "inputs/CoFusionReader.h"
+
 
 int main(int argc, const char* argv[]) 
 {
-  if (argc != 2) 
-  {
-    std::cerr << "usage: example-app <path-to-exported-script-module>\n";
-    return -1;
-  }
 
-  torch::jit::script::Module module;
-  try 
-  {
-    Deserialize the ScriptModule from a file using torch::jit::load().
-    module = torch::jit::load(argv[1]);
-  }
-  catch (const c10::Error& e) 
-  {
-    std::cerr << "error loading the model\n";
-    return -1;
-  }
-  
-  std::cout << "ok\n";
+  CoFusionReader cfreader(argv[1]);
+  cfreader.getNext();
+
+  return 0;
 }
