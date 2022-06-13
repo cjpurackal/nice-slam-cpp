5fc8b2a107b107d1c20aae8ff42e7e1c84d52325
cjpurackal <christeejacobs@gmail.com>
2022-06-13 18:55:53 +0530

    minor

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,12 @@
 //cjpurackal
 //June 11 '22, 16:54:00
-#include "Mapper.h"
+// #include "Mapper.h"
 #include "Tracker.h"
 #include "models/MLP.h"
 
 int main(int argc, const char* argv[])
 {
-	Mapper mapr;
+	// Mapper mapr;
 	Tracker tracker;
 	// mapr.run(argv[1]);
 
